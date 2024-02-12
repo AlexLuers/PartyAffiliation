245889ca6042e062a330039522545f5cf8da333b
AlexLuers <156149375+AlexLuers@users.noreply.github.com>
2024-02-12 16:12:05 -0500

    Update PartyAffiliation.java

--- PartyAffiliation/src/partyaffiliation/PartyAffiliation.java
+++ PartyAffiliation/src/partyaffiliation/PartyAffiliation.java
@@ -14,7 +14,26 @@ public class PartyAffiliation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      Scanner in = new Scanner(System.in);
+        String menuChoice = "";
+        //displaying the menu
+        String menu = "D = Democratic/tR = Republican/tI = Independant/nChoose you move: ";
+        System.out.print(menu);
+        menuChoice = in.nextLine();
+        //all the outputs
+        if(menuChoice.equalsIgnoreCase("B"))
+        {
+            System.out.println("You are a Democratic Donkey!");
+        }
+        else if(menuChoice.equalsIgnoreCase("R"))
+        {
+            System.out.println("You are a Republican Elephant!");
+        }
+        else if(menuChoice.equalsIgnoreCase("I"))
+        {
+            System.out.println("You are an Independant Person!");
+        }
+
     }
     
 }
